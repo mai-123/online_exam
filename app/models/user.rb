e761a703a20d50246bf5dfba5f2cbebf3454f83a
mai123 <pham.thi.mai@vareal.vn>
2019-07-15 16:58:28 +0700

    edit review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-  validates :name, presence: true, lenth: { maximum: 50, minimum: 3 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+
+  validates :name, presence: true, lenth: {maximum: 50, minimum: 3}
+  validates :email, presence: true, length: {maximum: 255},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 6}
 end
