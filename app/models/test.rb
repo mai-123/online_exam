a4116e97011665995c4c980d1b969a355506720a
mai123 <pham.thi.mai@vareal.vn>
2019-07-16 14:21:38 +0700

    refs#4 Create Test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,9 @@
 class Test < ApplicationRecord
   CATEGORY_TYPE = [:git, :rails]
+
   enum type: CATEGORY_TYPE
 
   validates :time, presence: true
+  validates :name, presence: true
+  validates :type, presence: true
 end
