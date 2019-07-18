a9fff72425910ca828bfc3c3bf5d9ba78ba3f319
mai123 <pham.thi.mai@vareal.vn>
2019-07-18 17:07:23 +0700

    close #17 Reset password

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,6 +7,6 @@ class UserMailer < ApplicationMailer
   #
   def password_reset user
     @user = user
-    mail to: @user.email, subject: I18n.t("subject")
+    mail to: @user.email, subject: t("user_mailer.password_reset.subject")
   end
 end
