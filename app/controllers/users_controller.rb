ebad2b02991080e1c026bb923c1f44712efdeda1
mai123 <pham.thi.mai@vareal.vn>
2019-07-22 16:56:40 +0700

    close #30 delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :check_is_admin_permission
+  before_action :get_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.paginate page: params[:page]
@@ -10,7 +11,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new user_params
     if @user.save
       flash[:success] = t ".success_create"
       redirect_to @user
@@ -20,15 +20,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find_by id: params[:id]
-    unless @user
-      flash[:error] = t ".error_404"
-      redirect_to root_path
-    end
   end
 
   def update
-    @user = User.find_by id: params[:id]
     if @user.update_attributes user_params
       flash[:success] = t ".success_update"
       redirect_to root_path
@@ -37,8 +31,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".success_delete"
+      redirect_to root_path
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def get_user
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:danger] = t ".error_404"
+      redirect_to root_path
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-User.create! name: "admin",
-  email: "admin@vareal.vn",
-  password: "123456",
-  password_confirmation: "123456",
-  is_admin: true
+# User.create! name: "admin",
+#   email: "admin@vareal.vn",
+#   password: "123456",
+#   password_confirmation: "123456",
+#   is_admin: true
 
-100.times do |n|
-  User.create! name: Faker::Name.name,
-    email: "user#{n + 1}@vareal.vn",
-    password: "123456",
-    password_confirmation: "123456"
-end
+# 100.times do |n|
+#   User.create! name: Faker::Name.name,
+#     email: "user#{n + 1}@vareal.vn",
+#     password: "123456",
+#     password_confirmation: "123456"
+# end
